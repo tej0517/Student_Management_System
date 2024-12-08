89c7ecd801d74f2cbf66ac23fe10219946bc4c4b
Asus <Asus@LAPTOP-DIVV4MEI>
2024-12-08 13:03:10 +0530

    first commit

---
+++ fullstack-backend/src/main/java/com/codewithtejas/fullstack_backend/entity/User.java
@@ -0,0 +1,61 @@
+package com.codewithtejas.fullstack_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String username;
+	private String name;
+	private String email;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	public User(Long id, String username, String name, String email) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.name = name;
+		this.email = email;
+	}
+	public User(String username, String name, String email) {
+		super();
+		this.username = username;
+		this.name = name;
+		this.email = email;
+	}
+	
+	
+	
+}
